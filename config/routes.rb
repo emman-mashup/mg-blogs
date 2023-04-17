91b2653d39116566696a4336e4c914476fbac3fe
emman <127918769+emman-mashup@users.noreply.github.com>
2023-04-17 13:06:57 +0800

    Added base auth views

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #TODO set to user homepage/feed instead of users list
+  root "users#index"
+
+  resources :users, only: [:index, :new, :edit, :create, :update, :destroy]
+
+  devise_scope :user do
+    get "/signout", to: "devise/sessions#destroy", as: :signout
+  end
+
 end
