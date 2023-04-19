1f726e47b8405bb62df5407f9048f6c303926856
emman <127918769+emman-mashup@users.noreply.github.com>
2023-04-19 12:33:08 +0800

    fixed migration syntax

--- db/migrate/20230418074513_create_blogs.rb
+++ db/migrate/20230418074513_create_blogs.rb
@@ -3,7 +3,7 @@ class CreateBlogs < ActiveRecord::Migration[7.0]
     create_table :blogs do |t|
       t.string :title
       t.text :content
-      t.integer :title :up_votes, default: 0
+      t.integer :up_votes, default: 0
       t.integer :down_votes, default: 0
 
       t.timestamps
