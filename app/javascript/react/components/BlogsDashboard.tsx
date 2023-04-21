3434dcb1e55f7b77b8f4e9323d74439fa89b7e48
ashleeeeeeyyyyyy <puaphee@gmail.com>
2023-04-21 16:49:50 +0800

    Changes in UI

--- app/javascript/react/components/BlogsDashboard.tsx
+++ app/javascript/react/components/BlogsDashboard.tsx
@@ -1,15 +1,25 @@
-import React, { useState, useEffect, ChangeEvent } from "react";
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import Avatar from 'boring-avatars';
 
 import {
   Blog,
   BlogBody,
   BlogAPIResponse,
   BlogListAPIResponse,
-} from "../types/blogTypes";
+} from '../types/blogTypes';
 
-import { fetchBlogs as defaultFetchBlogs } from "../../api/blog";
+import { fetchBlogs as defaultFetchBlogs } from '../../api/blog';
 
-import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  ChatBubbleLeftRightIcon,
+  ArrowSmallRightIcon,
+  PencilIcon,
+  TrashIcon,
+} from '@heroicons/react/24/outline';
+
+import '../styles.css';
 
 export type BlogsDashboardProps = {
   fetchBlogs?: () => Promise<BlogListAPIResponse>;
@@ -19,7 +29,7 @@ const BlogsDashboard = ({
   fetchBlogs = defaultFetchBlogs,
 }: BlogsDashboardProps) => {
   const [blogs, setBlogs] = useState([] as Blog[]);
-  console.log("blog dashboard");
+  console.log('blog dashboard');
   useEffect(() => {
     getBlogs();
   }, []);
@@ -32,25 +42,115 @@ const BlogsDashboard = ({
       });
       setBlogs(responseBlogs);
     } catch {
-      console.error("failed to get blogs");
+      console.error('failed to get blogs');
     }
   };
 
   return (
     <>
-      <div>
+      <div className="post-card rounded-3 bg-white mt-2">
         {/* votes */}
-        <div className="flex flex-col items-center justify-start space-y-1 rounded-l-md">
-          <ArrowUpIcon className="voteButtons" />
-          <p>0</p>
-          <ArrowDownIcon className="voteButtons" />
+        <div className="vote container left">
+          <div className="left-right">
+            <ArrowUpIcon className="voteButtons" />
+            <p className="spacing">1.1k</p>
+            <ArrowDownIcon className="voteButtons" />
+          </div>
+
+          <div className="left-down">
+            <div className="chat-style">
+              <ChatBubbleLeftRightIcon className="voteButtons" />
+              <p className="spacing">24</p>
+            </div>
+
+            <ArrowSmallRightIcon className="voteButtons" />
+            <PencilIcon className="voteButtons" />
+            <TrashIcon className="voteButtons" />
+          </div>
         </div>
 
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="right p-2 pb-1">
+          <div className="card-header">
+            <div className="avatar-style">
+              <Avatar
+                size={40}
+                name="Maria Mitchell"
+                variant="beam"
+                colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
+              />
+              <h5 className="spacing">Ashley Pua Phee</h5>
+            </div>
+
+            <p className="spacing">6 hours ago</p>
+          </div>
+
+          <div>
+            <p>@design-talks</p>
+          </div>
+
+          <div>
+            <h5>First UI/UX Session in MG Bootcamp 2023</h5>
+          </div>
+
+          <div>
+            <img
+              src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/f1aac9117592339.6078858c5ddba.jpg"
+              className="card-image"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="post-card rounded-3 bg-white mt-2">
+        {/* votes */}
+        <div className="vote container left">
+          <div className="left-right">
+            <ArrowUpIcon className="voteButtons" />
+            <p className="spacing">1.1k</p>
+            <ArrowDownIcon className="voteButtons" />
+          </div>
+
+          <div className="left-down">
+            <div className="chat-style">
+              <ChatBubbleLeftRightIcon className="voteButtons" />
+              <p className="spacing">24</p>
+            </div>
+
+            <ArrowSmallRightIcon className="voteButtons" />
+            <PencilIcon className="voteButtons" />
+            <TrashIcon className="voteButtons" />
+          </div>
+        </div>
+
+        <div className="right p-2 pb-1">
+          <div className="card-header">
+            <div className="avatar-style">
+              <Avatar
+                size={40}
+                name="Maria Mitchell"
+                variant="beam"
+                colors={['#92A1C6', '#146A7C', '#F0AB3D', '#C271B4', '#C20D90']}
+              />
+              <h5 className="spacing">Ashley Pua Phee</h5>
+            </div>
+
+            <p className="spacing">6 hours ago</p>
+          </div>
+
+          <div>
+            <p>@design-talks</p>
+          </div>
+
+          <div>
+            <h5>First UI/UX Session in MG Bootcamp 2023</h5>
+          </div>
+
+          <div>
+            <img
+              src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/f1aac9117592339.6078858c5ddba.jpg"
+              className="card-image"
+            />
+          </div>
         </div>
       </div>
     </>
